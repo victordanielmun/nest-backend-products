e1b436551b53d606832e21a7d56d282a0d68f6cd
victordanielmun <victordanielmun@gmail.com>
2024-11-06 08:55:36 -0500

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  await app.listen(process.env.PORT ?? 4000);
+}
+bootstrap();
---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,4 @@
+// export class CreateProductDto {}
+import { Product } from '@prisma/client';
+
+export type CreateProductDto = Omit<Product, 'id' | 'createdAt' | 'updatedAt'>;
